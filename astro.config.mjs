6967d34427aee83ecb9fca99a141517157dfc6ec
Gustavo Huarcaya <diavolo@gahd.net>
2026-02-25 22:50:27 -0500

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://http.biz',
+  integrations: [sitemap()],
+  redirects: {
+    '/status-codes': '/status',
+    '/status-codes/': '/status/',
+  },
+});
